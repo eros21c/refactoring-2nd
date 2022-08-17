5ff0eacbf521dd9e8728c23c76b8b73089541886
eros21c <eros21c@outlook.com>
2022-08-18 07:23:44 +0900

    데이터 생성 함수/파일 추출 Html Render 추가

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -1,70 +1,46 @@
-export {statement}
+import createStatementData from './createStatementData';
+
+export {statement, htmlStatement}
 
 function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice,plays));
+}
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            {
-                style: "currency", currency: "USD",
-                minimumFractionDigits: 2
-            }).format(aNumber/100);
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
     }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (playFor(aPerformance).type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-        }
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function volumeCreditsFor(perf) {
-        let result = Math.max(perf.audience - 30, 0);
-        if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
-        return result;
-    }
-
-    function totalAmount() {
-        let result = 0 ;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf);
-        }
-        return result;
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
-
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
-    for (let perf of invoice.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
     return result;
+}
+
+function renderPlainText(data) {
+
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber / 100);
 }
--- test/chp1/statement.test.js
+++ test/chp1/statement.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import {statement} from '../../src/chp1/statement.js'
+import {statement, htmlStatement} from '../../src/chp1/statement.js'
 
 describe('statement', () => {
     let playsJson =
@@ -39,5 +39,49 @@ describe('statement', () => {
         "적립 포인트: 47점\n";
 
         expect(statement(invoicesJson[0], playsJson)).to.equal(expected);
+    });
+});
+
+describe('htmlStatement', () => {
+    let playsJson =
+        {
+            "hamlet": {"name": "Hamlet", "type": "tragedy"},
+            "as-like": {"name": "As You Like It", "type": "comedy"},
+            "othello": {"name": "Othello", "type": "tragedy"}
+        };
+
+    let  invoicesJson =
+        [
+            {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "hamlet",
+                        "audience": 55
+                    },
+                    {
+                        "playID": "as-like",
+                        "audience": 35
+                    },
+                    {
+                        "playID": "othello",
+                        "audience": 40
+                    }
+                ]
+            }
+        ];
+
+    it('should print a htmlStatement for multiple plays, single customer and multiple seats in html', () => {
+        let expected = "<h1>청구 내역 (고객명: BigCo)</h1>\n" +
+            "<table>\n" +
+            "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>" +
+            "  <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>\n" +
+            "  <tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td></tr>\n" +
+            "  <tr><td>Othello</td><td>(40석)</td><td>$500.00</td></tr>\n" +
+            "</table>\n" +
+            "<p>총액: <em>$1,730.00</em></p>\n" +
+            "<p>적립 포인트: <em>47</em>점</p>\n";
+
+        expect(htmlStatement(invoicesJson[0], playsJson)).to.equal(expected);
     });
 });
