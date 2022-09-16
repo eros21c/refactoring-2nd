3a6f5c98fffd7c604e346330a8c1c34e8a748560
eros21c <eros21c@outlook.com>
2022-09-17 00:19:24 +0900

    ProductionPlan & Test 추가

---
+++ test/chp4/productionPlan.test.js
@@ -0,0 +1,75 @@
+import { assert, expect } from 'chai';
+import {Province, sampleProvinceData} from '../../src/chp4/productionPlan'
+
+describe('province', function () {
+   let asia;
+   beforeEach(function(){
+      asia = new Province(sampleProvinceData());
+   });
+
+   it('shortfall', function() {
+     assert.equal(asia.shortfall, 5);
+   });
+
+   it('profit', function() {
+      expect(asia.profit).equal(230);
+   });
+
+   it('change production', function() {
+       asia.producers[0].production = 20;
+       expect(asia.shortfall).equal(-6);
+       expect(asia.profit).equal(292);
+   });
+
+    it('zero demand', function() {
+        asia.demand = 0;
+        expect(asia.shortfall).equal(-25);
+        expect(asia.profit).equal(0);
+    });
+
+    it('empty string demand', function() {
+       asia.demand = "";
+       expect(asia.shortfall).NaN;
+       expect(asia.profit).NaN;
+    });
+
+    /*it('negative demand', function() {
+        asia.command = -1;
+        expect(asia.shortfall).equal(-26);
+        expect(asia.profit).equal(-10);
+    });*/
+});
+
+describe('no producers', function() {
+    let noProducers;
+    beforeEach(function() {
+        const data = {
+           name: "No producers",
+           producers: [],
+           demand: 30,
+           price: 20
+        };
+        noProducers = new Province(data);
+    });
+    it('shortfall', function() {
+        expect(noProducers.shortfall).equal(30);
+    });
+    it('profit', function() {
+        expect(noProducers.profit).equal(0);
+    })
+
+});
+
+describe('string for producers', function () {
+    it('', function () {
+        const data = {
+            name: "String producers",
+            producers: "",
+            demand: 30,
+            price: 20
+        };
+        expect(function() {
+            new Province(data);
+        }).to.throw(TypeError);
+    });
+});
